2078fccddc875e6aab702d7a8c939ebdcce8d8e6
root <maryam.sokkary@gmail.com>
2016-12-08 21:49:14 +0200

    bug fixs

---
+++ AndroidStudioProjects/Jisho.org/app/src/main/java/com/example/maryam/jishoorg/allInfo.java
@@ -0,0 +1,13 @@
+package com.example.maryam.jishoorg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class allInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_info);
+    }
+}
